e3169841fca17ec9a3183c0798b5ecac33f9557e
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-05-29 10:01:06 -0500

    trying to add attributes to the <p> element

---
+++ editable-content.js
@@ -0,0 +1,60 @@
+var myApp = angular.module('editableContentMod', []);
+
+myApp.run(function($rootScope) {
+});
+
+myApp.directive('editableContent', function() {
+   return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: './editable-content.html'
+   }
+});
+
+myApp.directive('makeEditable', function() {
+   return {
+      restrict: 'A',
+      scope: true,
+      link: function(scope, element, attrs) {
+         //
+         //
+         console.log('star of makeEditable');
+         //
+         //
+         var textBlock = element.find('p')[0];
+         console.log(textBlock);
+         
+         scope.buttonState = "Edit";
+         scope.toggleEdit = function() {
+            console.log('toggleEdit');
+            scope.buttonState == "Edit" ? enterEditMode() : exitEditMode();
+         };
+
+         function enterEditMode() {
+            //
+            console.log('start of enterEditMode');
+            //
+            scope.buttonState = "Save";
+            // .attr('contenteditable', 'true');
+            //
+            console.log('end of enterEditMode');
+            //
+         }
+         function exitEditMode() {
+            //
+            console.log('start of exitEditMode');
+            //
+            scope.buttonState = "Edit";
+            // .attr('contenteditable', 'false');
+            //
+            console.log('end of exitEditMode');
+            //
+         }
+         //
+         //
+         console.log('end of makeEditable');
+         //
+         //
+      }
+   }
+});
